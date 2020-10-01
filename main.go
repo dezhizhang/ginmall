031aa0e89786f48b57e5e9a191bd4b9ff4c1a56f
zhangdezhi <1541609448@qq.com>
2020-10-01 20:31:04 +0800

    修改

--- main.go
+++ main.go
@@ -4,18 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// func main() {
-// 	r := gin.Default();
-// 	r.GET("/hello",func(c *gin.Context) {
-// 		c.JSON(200,gin.H{
-// 			"code":200,
-// 			"msg":"成功",
-// 			"success":true,
-// 		})
-// 	});
-// 	r.Run(":8082")
-// }
-
 func main() {
 	router := gin.Default();
 
