998deb2815cb449c71c99a7480aafcaf6e356384
zhangdezhi <1541609448@qq.com>
2020-10-01 22:51:46 +0800

    连接数据库

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"ginApi/src/controller"
+	"github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
+
+
 	router := gin.Default();
+	db, err := gorm.Open("mysql", "root:701XTAY1993@/beego?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		fmt.Println("数据库连接失败")
+		return
+	}
 
 	//分组
 	v1 := router.Group("/api/v1/topics")
@@ -17,6 +27,7 @@ func main() {
 	}
 	
 
-	router.Run();
+	router.Run()
+	defer db.Close()
 
 }
---
+++ src/utils/db.go
@@ -0,0 +1,7 @@
+// package utils
+
+// import (
+// 	"github.com/jinzhu/gorm"
+//     _ "github.com/jinzhu/gorm/dialects/mysql"
+// )
+
