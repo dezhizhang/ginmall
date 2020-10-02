2e381962b951cc68efdfb5b33e9b57af706b3007
zhangdezhi <1541609448@qq.com>
2020-10-02 17:21:18 +0800

    提交

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.14
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gomodule/redigo v1.8.2
 	github.com/jinzhu/gorm v1.9.16
 )
--- main.go
+++ main.go
@@ -1,30 +1,38 @@
 package main
 
 import (
-	"ginApi/src/utils"
-	"github.com/gin-gonic/gin"
+	"fmt"
 	"ginApi/src/controller"
+	"ginApi/src/utils"
 
+	"github.com/gin-gonic/gin"
+	"github.com/gomodule/redigo/redis"
 )
 
 func main() {
-	
-	
-	router := gin.Default();
-	
+
+	router := gin.Default()
+
+	conn := utils.RedisDefaultPool.Get()
+	ret, err := redis.String(conn.Do("get", "username"))
+	if err != nil {
+		fmt.Println("出错了", err)
+		return
+	}
+	fmt.Println("+++")
+	fmt.Println(ret)
+	fmt.Println("+++++")
 
 	//分组
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("",controller.GetTopList)
-		v1.POST("/create",controller.GetTopCreate)
-		v1.POST("/user/add",controller.UserAdd)
-		v1.GET("/user/info",controller.GetUser)
-		v1.GET("/user/info/:id",controller.GetUserOne)
+		v1.GET("", controller.GetTopList)
+		v1.POST("/create", controller.GetTopCreate)
+		v1.POST("/user/add", controller.UserAdd)
+		v1.GET("/user/info", controller.GetUser)
+		v1.GET("/user/info/:id", controller.GetUserOne)
 	}
 	defer utils.DB.Close()
-	router.Run();
-
-	
+	router.Run()
 
 }
