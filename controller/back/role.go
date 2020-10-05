663f2804842bc6742aaa7a3c17d031b6059200b8
zhangdezhi <1541609448@qq.com>
2020-10-05 14:42:48 +0800

    功能完善

--- controller/back/role.go
+++ controller/back/role.go
@@ -9,7 +9,15 @@ import (
 )
 
 func Role(c *gin.Context) {
-	c.HTML(200, "back/roleList.html", nil)
+	roleList := []model.Role{}
+	err := utils.DB.Find(&roleList).Error
+	if err != nil {
+		fmt.Println("获取数据失败", err)
+		return
+	}
+	c.HTML(200, "back/roleList.html", gin.H{
+		"roleList": roleList,
+	})
 }
 
 func RoleAdd(c *gin.Context) {
@@ -17,15 +25,15 @@ func RoleAdd(c *gin.Context) {
 }
 
 func RoleDoAdd(c *gin.Context) {
-	query := model.Role{}
-	err := c.Bind(&query)
+	role := model.Role{}
+	err := c.Bind(&role)
 	if err != nil {
 		fmt.Println("绑定失败", err)
 		return
 	}
-	query.AddTime = int(utils.GetUnix())
-	query.Status = 1
-	err = utils.DB.Create(&query).Error
+	role.AddTime = int(utils.GetUnix())
+	role.Status = 1
+	err = utils.DB.Create(&role).Error
 	if err != nil {
 		fmt.Println("插入数据失败", err)
 		return
